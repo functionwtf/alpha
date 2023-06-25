1b8b5290c47478d6eeb324a492e99fc5aab25e31
0x00badcode <paul.crinquand@epitech.eu>
2023-06-25 19:08:02 +0200

    added front-end part

---
+++ frontend/openDoor.js
@@ -0,0 +1,52 @@
+async function connectWallet() {
+    if (window.ethereum) {
+        try {
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+            return new ethers.providers.Web3Provider(window.ethereum);
+        } catch (error) {
+            console.error("User denied account access:", error);
+            alert("Unable to connect to wallet. User denied account access.");
+        }
+    } else {
+        console.error("Non-Ethereum browser detected. Please consider installing MetaMask");
+        alert("Non-Ethereum browser detected. Please consider installing MetaMask");
+    }
+}
+
+async function createTransaction() {
+    const provider = await connectWallet();
+    if (!provider) return;
+
+    const signer = provider.getSigner();
+    const transaction = {
+        to: await signer.getAddress(),
+        value: ethers.utils.parseEther("0.0"),
+    };
+
+    try {
+        const signedTransaction = await signer.signTransaction(transaction);
+
+        const response = await fetch('http://future-server-url/receive-transaction', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ rawTransaction: signedTransaction })
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        } else {
+            const responseData = await response.text();
+            console.log(responseData);
+            alert(responseData);
+        }
+    } catch (error) {
+        console.error("Error while creating and sending transaction:", error);
+        alert("Error while creating and sending transaction: " + error.message);
+    }
+}
+
+window.onload = function() {
+    document.getElementById('createTransaction').addEventListener('click', createTransaction);
+}
